8e7472719823549526a74fa4c52048aea5dfaaa5
fredo <fredi.vainumae1@gmail.com>
2020-03-15 14:51:31 +0200

    button click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,30 @@ $(document).ready(function () {
     $(".member").removeClass("member-hidden")
   });
 });
+
+// Delete button after click
+
+$(".member-button").on('click', function () {
+  $(".member").addClass("member-after");
+  $(this).remove();
+});
+
+
+
+
+
+
+
+// Show more brands
+
+$(document).ready(function () {
+  $("#btn2").on('click', function () {
+    $(".brand-item").removeClass("brand-hidden")
+  });
+});
+
+// Remove brand button after click
+
+$(".brand-button").on('click', function () {
+  $(this).remove();
+});
